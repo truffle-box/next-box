16ade6d5b5b8ea5aaea173af58aa2224207328e4
Cliff Zhang <cliffzhang08@gmail.com>
2023-03-21 18:57:40 +0000

    client: Install web3 v4

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          net: false,
+          fs: false
+        }
+      };
+    }
+    return config;
+  }
 };
 
 module.exports = nextConfig;
